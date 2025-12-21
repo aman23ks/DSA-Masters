d6da29156b11be555d7f00271a6e2af4fbed0a12
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2025-12-21 09:17:36 +0530

    Time: 40 ms (35.97%), Space: 34.5 MB (94.99%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,38 +6,51 @@
 
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
+        
+        if head == None or head.next == None:
             return True
-
-        # 1) Find end of first half (slow) using fast/slow pointers
-        slow, fast = head, head
-        while fast.next and fast.next.next:
+        
+        # Find length of linked list
+        length = 0
+        curr_head = head
+        while curr_head:
+            length += 1
+            curr_head = curr_head.next
+        
+        # Find the mid point of the linked list 
+        slow = head
+        fast = head
+        prev = head
+         
+        while fast and fast.next:
+            prev = slow
             slow = slow.next
             fast = fast.next.next
-
-        # 2) Reverse second half starting at slow.next
-        second = self._reverse(slow.next)
-
-        # 3) Compare first half and reversed second half
-        p1, p2 = head, second
+        
+        if fast:
+            second_start = slow.next
+            slow.next = None
+        else:
+            second_start = slow
+            prev.next = None
+            
+        prev_rev = None
+        curr = second_start
+        while curr:
+            nxt = curr.next
+            curr.next = prev_rev
+            prev_rev = curr
+            curr = nxt
+        
+        p1 = head
+        p2 = prev_rev
+        
         while p2:
             if p1.val != p2.val:
-                # Optional restore before returning
-                slow.next = self._reverse(second)
                 return False
             p1 = p1.next
             p2 = p2.next
-
-        # 4) Optional: restore list
-        slow.next = self._reverse(second)
+        
         return True
-
-    def _reverse(self, node: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        cur = node
-        while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
-        return prev
+            
+        
