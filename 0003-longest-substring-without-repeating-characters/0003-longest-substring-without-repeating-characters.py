4c2a264f779d78640102f3f2b26ab3471e7f6d9c
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2026-02-03 15:30:52 -0700

    Time: 19 ms (39.31%), Space: 19.3 MB (36.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,17 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 1:
-            return 1
-        
-        maxlen = 0
-        hashmap = {}
-        start = 0  # To keep track of the start of the window
-        
-        for i in range(len(s)):
-            if s[i] in hashmap and hashmap[s[i]] >= start:
-                start = hashmap[s[i]] + 1  # Move the start to the right of the previous occurrence
-            
-            hashmap[s[i]] = i  # Update the last seen index of the character
-            maxlen = max(maxlen, i - start + 1)  # Update the max length of the window
-
-        return maxlen
+        st = ""
+        i = 0
+        length = 0
+        maxi = 0
+        while i < len(s):
+            if s[i] in st:
+                while st[0] != s[i]:
+                    st = st[1:]
+                    length -= 1
+            if st != "" and st[0] == s[i]:
+                st = st[1:]
+                length -= 1
+            st += s[i]
+            i += 1
+            length += 1
+            maxi = max(length, maxi)
+        return maxi
